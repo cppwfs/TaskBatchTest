bd8b7f4e16913d0bfa57f34a5aca2ffcb192b904
Glenn Renfro <grenfro@pivotal.io>
2016-03-07 15:51:54 -0500

    Introduction to source control.

---
+++ src/main/java/org/springframework/cloud/task/TestTaskBatchApplication.java
@@ -0,0 +1,12 @@
+package org.springframework.cloud.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskBatchApplication.class, args);
+	}
+}
